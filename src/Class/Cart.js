dd73a01f632d627b905ccbbe445a8bebe41f36e8
Celeste De Los Santos <celestedelossantos98@gmail.com>
2024-05-08 19:37:12 -0300

    fixes en endpoint de cart y typos

--- src/Class/Cart.js
+++ src/Class/Cart.js
@@ -10,27 +10,29 @@ export class CartManager {
     async getCarts(){
         const response = await fs.readFile(this.path, 'utf-8')
         this.carts = [...JSON.parse(response).data]
-        return [...this.products]
+        return [...this.carts]
     }
 
-    async newCart(cart){
-        this.carts = this.getCarts();
-        this.carts.push({ id: uuidv4(), products: cart });
+    async newCart(){
+        const newCart = { id: uuidv4(), products: [] }
+        this.carts = await this.getCarts();
+        this.carts.push(newCart);
         await fs.writeFile(this.path, JSON.stringify({ data: this.carts }))
+        return newCart
     }
 
     async getCartById(id){
-        this.carts = this.getCarts();
+        this.carts = await this.getCarts();
         const cartFinded = this.carts.find( cart => cart.id === id)
 
         return cartFinded || console.log('Not Found') 
     }
 
     async addProductToCard(id, productId){
-        this.carts = this.getCarts();
+        this.carts = await this.getCarts();
         const cardsUpdated = this.carts.map((cart)=>{
             if(cart.id === id){
-                const indexProd = cart.products.indexOf(prod => prod.id === productId);
+                const indexProd = cart.products.findIndex(prod => prod.id === productId);
                 if(indexProd === -1){
                     cart.products.push({ id: productId, quantity: 1 })
                     return cart;
--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,34 +1,31 @@
 import { Router } from 'express';
-import { CartManager } from '../Class/Cart';
+import { CartManager } from '../Class/Cart.js';
 
 const router = Router();
 const cartsManager = new CartManager('./src/data/databasecarts.json');
 
-router.use(express.json())
-
 router.post('/', async (req, res) => {
-    const newCart = req.body
-    await cartsManager.newCart(newCart)
+    const newCart = await cartsManager.newCart()
 
-    res.status(201).json({ message: 'Save is successfully'})
+    res.status(201).json({ message: 'Save is successfully', cartId: newCart.id })
 });
 
 router.get('/:cid', async (req, res) => {
     const { cid } = req.params
 
-    const productList = await cartsManager.getCartById(cid);
+    const cartFinded = await cartsManager.getCartById(cid);
 
-    if(!productList) res.status(404).json({ message: 'Not Found' })
+    if(!cartFinded) res.status(404).json({ message: 'Not Found' })
 
-    res.json(productList)
+    res.json({ productList: cartFinded.products })
 });
 
 router.post('/:cid/product/:pid', async (req, res) => {
     const { cid, pid } = req.params
-
+    
     await cartsManager.addProductToCard(cid, pid);
 
-    res.json({message: 'Producto Agregado'})
+    res.json({message: 'Product Added'})
 
 });
 
